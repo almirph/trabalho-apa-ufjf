09e4fa8d6ed74b275e636630a26dd807c46a99ae
almirph <phelipear@gmail.com>
2022-01-03 10:51:36 -0300

    Questão 3

--- main.cpp
+++ main.cpp
@@ -67,15 +67,15 @@ vector<vector<int>> Questao1()
     return GeraGrafo_Q1(a, v);
 }
 
-void Questao2(vector<vector<int>> arestas)
+vector<vector<int>> Questao2(vector<vector<int>> arestas)
 {
     cout<<"\nMatriz de adjacencias: \n";
     int i, j, contador;
     vector<vector<int>> matriz(100, vector<int>(100, 0));
     for(i = 0; i < arestas.size(); i++)
     {
-            matriz.at(arestas.at(i).at(0) - 1 ).at(arestas.at(i).at(1) - 1 ) = 1;
-            matriz.at(arestas.at(i).at(1) - 1 ).at(arestas.at(i).at(0) - 1 ) = 1;
+        matriz.at(arestas.at(i).at(0) - 1 ).at(arestas.at(i).at(1) - 1 ) = 1;
+        matriz.at(arestas.at(i).at(1) - 1 ).at(arestas.at(i).at(0) - 1 ) = 1;
     }
 
     for(i = 0; i < 100; i++)
@@ -86,8 +86,36 @@ void Questao2(vector<vector<int>> arestas)
             cout << " " << matriz.at(i).at(j) << " ";
         }
     }
+
+    return matriz;
 }
-void Questao3() {}
+
+void Questao3(vector<vector<int>> matriz)
+{
+    vector<int> vetor(4950);
+    int i, j;
+    int contador = 0;
+
+    for(i=0; i<100; i++)
+    {
+        for(j=0; j<100; j++)
+        {
+            if(i < j)
+            {
+                vetor.at(contador) = matriz.at(i).at(j);
+                contador ++;
+            }
+        }
+    }
+
+    cout<<"\nVetor Binario: \n";
+
+    for(i=0; i<4950; i++)
+    {
+        cout<<" "<< vetor[i] << " ";
+    }
+
+}
 void Questao4() {}
 void Questao5() {}
 void Questao6() {}
@@ -111,7 +139,7 @@ int main()
         break;
 
     case '3':
-        Questao3();
+        Questao3(Questao2(Questao1()));
         break;
 
     case '4':
